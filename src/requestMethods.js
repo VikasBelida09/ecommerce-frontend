97706630da600565f32e922d21ec35c3124cd0d1
vikas belida <vikasbelida@vikass-Air.lan>
2022-12-31 17:27:36 -0500

    bug fix

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL=process.env.NODE_ENV==='production'?'https://ecommerce-backend-smvy.onrender.com/api/':'http://localhost:3001/api/';
+const BASE_URL='https://ecommerce-backend-smvy.onrender.com/api/'
 const token=JSON?.parse(JSON?.parse(localStorage?.getItem("persist:root") || '{}')?.user || '{}')?.currentUser?.accessToken;
 
 export const publicRequest=axios.create({
